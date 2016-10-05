245e8fabfb22b9951c5c7bb58bf995db37368bfd
Peter Karman <peter.karman@gsa.gov>
2016-10-05 10:19:50 -0500

    Add devise_zxcvbn server-side password validation
    **Why**: Match password validation rules on the server
    with same library used on the client.
    
    **How**: The configuration calls for a level of 3 out of 4
    (Good) password strength. We can adjust as necessary.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,20 @@
 AppSetting.find_or_create_by!(name: 'RegistrationsEnabled') do |setting|
   setting.value = '1'
 end
+<<<<<<< 04872241cbddec3bcff4944d44c541529ebc79af
+=======
+
+if Rails.env.development?
+  # Create a few dummy accounts for use during development.  These accounts all
+  # have 'password' as password and are setup for phone OTP delivery.
+  %w(test1@test.com test2@test.com).each_with_index do |email, index|
+    User.find_or_create_by!(email: email) do |user|
+      user.skip_confirmation!
+      user.reset_password('a long password!', 'a long password!')
+      user.phone = format('+1 (415) 555-01%02d', index)
+      user.phone_confirmed_at = Time.current
+      Event.create(user_id: user.id, event_type: :account_created)
+    end
+  end
+end
+>>>>>>> Add devise_zxcvbn server-side password validation
